66ff31f690da7debf83bc79a1ae46c9e950ffbb1
Akib <cadetakib1689@gmail.com>
2024-04-02 00:28:31 +0600

    Changed the profile div to card

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,30 +1,45 @@
 // file to hold the left portion of the profile page
 
 import * as React from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
+
+// profile avatar
+import { 
+    Avatar, 
+    AvatarFallback, 
+    AvatarImage 
+} from "@/components/ui/avatar"
+
+// card component
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card"
+import { cn } from '@/lib/utils';
+
+type CardProps = React.ComponentProps<typeof Card>;
+
+
+// env variables
+const { NEXT_PUBLIC_FACEBOOK, NEXT_PUBLIC_LINKEDIN } = process.env;
 
 export function ProfileLeft() {
   return (
-    <div className='flex flex-col justify-center p-3 space-y-4'>
-        <div className='flex justify-center'>
+    <Card className="w-[350px]">
+        <div className='flex justify-center pt-5 p-3'>
             <Avatar className='w-48 h-48'>
                 <AvatarImage src="/images/profile_img.jpg" />
                 <AvatarFallback>Akib</AvatarFallback>
             </Avatar>
         </div>
-        
-        <div className='flex flex-col justify-center'>
-            <h1 className='text-2xl font-bold lg:text-3xl'>Akibur Rahman</h1>
-            <p>Jr. Software Engineer | <a href='https://pridesys.com'>Pridesys IT</a> </p>
-        </div>
-
-        <div className='flex flex-col justify-center space-y-2'>
-            <p>📍 Dhaka, Bangladesh</p>
-            <p>📧 <a href='mailto:cadetakib1689+profile@gmail.com'> cadetakib1689@gmail.com</a> </p>
-            <p>📞</p>
-            <p>🌐</p>
-        </div>
-    </div>
+      <CardHeader>
+        <CardTitle>Akibur Rahman</CardTitle>
+        <CardDescription>Full Stack Developer</CardDescription>
+      </CardHeader>
+    </Card>
     );
 }
