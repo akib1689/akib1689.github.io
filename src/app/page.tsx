109987e2531a6d5550bec28dd047dfcf51cc2285
Akib <cadetakib1689@gmail.com>
2024-04-01 09:17:22 +0600

    Added profile image with avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import {Button} from "@/components/ui/button"
 import { ModeToggle } from "@/components/mode-toggle";
+import { ProfileLeft } from "@/components/profile";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <ProfileLeft />
+      </div>
       <Button>Click me</Button>
       <ModeToggle />
     </main>
