19579f27a248cb7b4eeade344982d951757ac71a
Akib <cadetakib1689@gmail.com>
2024-04-01 10:08:17 +0600

    started working on filing up the details

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -6,10 +6,25 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export function ProfileLeft() {
   return (
-    // profile image
-    <Avatar className='w-48 h-48'>
-        <AvatarImage src="/images/profile_img.jpg" />
-        <AvatarFallback>Akib</AvatarFallback>
-    </Avatar>
+    <div className='flex flex-col justify-center p-3 space-y-4'>
+        <div className='flex justify-center'>
+            <Avatar className='w-48 h-48'>
+                <AvatarImage src="/images/profile_img.jpg" />
+                <AvatarFallback>Akib</AvatarFallback>
+            </Avatar>
+        </div>
+        
+        <div className='flex flex-col justify-center'>
+            <h1 className='text-2xl font-bold lg:text-3xl'>Akibur Rahman</h1>
+            <p>Jr. Software Engineer | <a href='https://pridesys.com'>Pridesys IT</a> </p>
+        </div>
+
+        <div className='flex flex-col justify-center space-y-2'>
+            <p>📍 Dhaka, Bangladesh</p>
+            <p>📧 <a href='mailto:cadetakib1689+profile@gmail.com'> cadetakib1689@gmail.com</a> </p>
+            <p>📞</p>
+            <p>🌐</p>
+        </div>
+    </div>
     );
 }
