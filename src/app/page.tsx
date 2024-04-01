ae911d29df737d9f5744f4fa8ec78e9b39f6a840
Akib <cadetakib1689@gmail.com>
2024-04-01 06:56:35 +0600

    added the theme toogling button but it is not working with dropdown button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import * as React from "react";
 import {Button} from "@/components/ui/button"
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Click me</Button>
+      <ModeToggle />
     </main>
   );
 }
