05e80afdf17f3dd8167c97540131cd62527ebaa4
Akib <cadetakib1689@gmail.com>
2024-04-06 07:38:40 +0600

    designed teh project card.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,25 +1,81 @@
 "use client";
 import * as React from "react";
-import { ProfileLeft } from "@/components/left-profile-component";
-import { MenubarComponent } from "@/components/navigation";
-import { useMediaQuery } from "@/hooks/use-media-query";
-import { About } from "@/components/about";
+
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
+import { projects } from "@/lib/projects";
+import { 
+  ArrowTopRightIcon, 
+  GitHubLogoIcon,
+  Link2Icon,
+} from "@radix-ui/react-icons";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+
+import { useTheme } from "next-themes";
 
 export default function Home() {
-  const isDesktop = useMediaQuery("(min-width: 1208px)");
+
+  const {theme } = useTheme();
+
   return (
     <>
-      <main className={isDesktop ? "flex justify-center p-8 h-fit" : "flex flex-col justify-center p-8 h-fit"}>
-        {isDesktop ? (
-          <>
-           Projects
-          </>
-        ) : (
-          <>
-            Projects
-          </>
-        )}
-      </main>
+      <div className="flex flex-col gap-5 h-fit">
+      {projects.map((project, i) => (
+          <Card key={i} className="group flex overflow-hidden">
+              <div className="w-fit flex justify-end">
+              {project.images.map((image, i) => (
+                  theme === image.theme ? (
+                    <Image 
+                      key={i}
+                      src={image.src}
+                      alt={project.title}
+                      width={400}
+                      height={250}
+                      className="group-hover:rotate-6 transition-all duration-300 ease-in-out"
+                    />
+                  ) : null
+                ))}
+              </div>
+              <div>
+                <CardHeader>
+                  <CardTitle>{project.title}</CardTitle>
+                </CardHeader>
+                <CardContent className="flex pr-0">
+                  <div className="flex flex-col gap-4 w-full">
+                    {project.description}
+                    <div className="flex gap-3">
+                    {project.links.map((link, i) => (
+                      <Button key={i} variant='link' className="gap-1">
+                        {link.image && link.image === "github" ? (<GitHubLogoIcon />) : (<Link2Icon />)}
+                        <a href={link.url} target="_blank" rel="noopener noreferrer">
+                          {link.name}
+                        </a>
+                        <ArrowTopRightIcon />
+                      </Button>
+                    ))}
+                    </div>
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <div className="flex gap-2">
+                    {project.tags.map((tag, i) => (
+                      <Badge key={i} variant='secondary'>{tag}</Badge>
+                    ))}
+                  </div>
+                </CardFooter>
+              </div>
+          </Card>
+        ))}
+      </div>
     </>
   );
 }
--- src/lib/projects.tsx
+++ src/lib/projects.tsx
@@ -1,15 +1,17 @@
 export const projects = [
     {
         title: "Example Title",
-        description: "lorem ipsum doller sit amet",
+        description: "lorem ipsum doller sit amet lorem ipsum doller sit amet lorem ipsum doller sit amet lorem ipsum doller sit amet",
         links : [
             {
                 name: "Github Repository",
-                url: "https://github.com/akib-89/akib-89"
+                url: "https://github.com/akib-89/akib-89",
+                image: "github",
             },
             {
                 name: "Live Demo",
-                url: "https://github.com/akib-89"
+                url: "https://github.com/akib-89",
+                image: "demo",
             }
         ],
         tags: [
@@ -17,10 +19,70 @@ export const projects = [
         ],
         images: [
             {
-                light: "images/profile_img.jpg"
+                theme: "light",
+                src: "images/profile_img.jpg",
             },
             {
-                dark: "images/profile_img.jpg"
+                theme: "dark", 
+                src: "images/profile_img.jpg"
+            }
+        ]
+    },
+    {
+        title: "Example Title",
+        description: "lorem ipsum doller sit amet lorem ipsum doller sit amet lorem ipsum doller sit amet lorem ipsum doller sit amet",
+        links : [
+            {
+                name: "Github Repository",
+                url: "https://github.com/akib-89/akib-89",
+                image: "github",
+            },
+            {
+                name: "Live Demo",
+                url: "https://github.com/akib-89",
+                image: "demo",
+            }
+        ],
+        tags: [
+            "README", "Markdown"
+        ],
+        images: [
+            {
+                theme: "light",
+                src: "images/profile_img.jpg",
+            },
+            {
+                theme: "dark", 
+                src: "images/profile_img.jpg"
+            }
+        ]
+    },
+    {
+        title: "Example Title",
+        description: "lorem ipsum doller sit amet lorem ipsum doller sit amet lorem ipsum doller sit amet lorem ipsum doller sit amet",
+        links : [
+            {
+                name: "Github Repository",
+                url: "https://github.com/akib-89/akib-89",
+                image: "github",
+            },
+            {
+                name: "Live Demo",
+                url: "https://github.com/akib-89",
+                image: "demo",
+            }
+        ],
+        tags: [
+            "README", "Markdown"
+        ],
+        images: [
+            {
+                theme: "light",
+                src: "images/profile_img.jpg",
+            },
+            {
+                theme: "dark", 
+                src: "images/profile_img.jpg"
             }
         ]
     }
