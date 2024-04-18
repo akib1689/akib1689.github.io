ba31c4695be71656a517b22d81404678a516ec5a
Akib <cadetakib1689@gmail.com>
2024-04-18 13:58:45 +0600

    added the experience

---
+++ src/lib/experience.tsx
@@ -0,0 +1,53 @@
+import exp from "constants";
+
+// file that contains the experience 
+export const experience = [
+    {
+        title: "Junior Software Engineer",
+        company: {
+            name: "Pridesys IT Ltd",
+            url: "https://www.linkedin.com/company/pridesys-it-ltd/",
+        },
+        location: "Dhaka, Bangladesh",
+        startDate: "November 2023",
+        endDate: "Present",
+        job_type: "Part-time",
+        description: [
+            "Contributed significantly to the development of a strong Spring microservice intended to handle user accounts, improve system speed, and improve user experience",
+            "Implemented extensive JUnit testing protocols to guarantee the microservice's functionality and dependability.",
+            "Automated GitHub Actions routines to enforce strict code quality requirements.",
+        ],
+    },
+    {
+        title: "Frontend Developer",
+        company: {
+            name: "Durjoy DSS",
+            url: "https://www.linkedin.com/company/durjoy/",
+        },
+        location: "Dhaka, Bangladesh",
+        startDate: "July 2021",
+        endDate: "January 2022",
+        job_type: "Remote",
+        description: [
+            "Developed a mobile application using Java and Android Studio that allowed users to track their daily expenses and income.",
+            "Designed and implemented a user-friendly interface that allowed users to easily input and view their financial data.",
+            "Published two applications on the Google Play Store, which have played a significant role in the company's growth.",
+        ],
+    },
+    {
+        title: "Content Reviewer",
+        company: {
+            name: "10 Minute School",
+            url: "https://www.linkedin.com/company/10ms/",
+        },
+        location: "Dhaka, Bangladesh",
+        startDate: "August 2020",
+        endDate: "September 2021",
+        job_type: "Contract",
+        description: [
+            "Reviewed and edited educational content to ensure accuracy and quality.",
+            "Collaborated with the content development team to create engaging and informative educational materials.",
+            "Provided feedback and suggestions to improve the quality of the content.",
+        ],
+    }
+];
