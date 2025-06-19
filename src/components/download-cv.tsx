0623aff0d1d8736811f3b3d1de7ac0518c8ff2fd
Akibur Rahman <54747148+akib1689@users.noreply.github.com>
2025-06-19 22:01:58 +0600

    update CV references and improve .gitignore for pyenv

--- src/components/download-cv.tsx
+++ src/components/download-cv.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 import { Button } from '@/components/ui/button'
-import { DownloadIcon } from '@radix-ui/react-icons'
 import { cn } from '@/lib/utils'
+import { DownloadIcon } from '@radix-ui/react-icons'
 
 type DownloadCvProps =  {
     className?: string;
@@ -12,7 +11,7 @@ const DownloadCv = (props: DownloadCvProps) => {
   return (
     <Button variant='default' className={cn('flex w-fit h-fit px-5 py-3 gap-4 items-center justify-center', props.className)}>
         <DownloadIcon className='w-6 h-6' />
-        <a href='/Akibur_Rahman_CV.pdf' download>
+        <a href='/Akibur_Rahman_Resume.pdf' download>
             Download CV
         </a>
     </Button>
